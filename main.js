3ef4067c3b7ddfc236be8fe294b09eb02fdc30bd
Leslie Rose <rosel5@unlv.nevada.edu>
2016-06-25 20:45:28 -0700

    Added Comments to code.

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-var serialport = require('serialport');
-var SerialPort = serialport.SerialPort;
-var port = new SerialPort(process.argv[2], {
+var serialport = require('serialport');  //load serialport module
+var SerialPort = serialport.SerialPort;  //
+var port = new SerialPort(process.argv[2], { //open serial port on specified port with a 500k baudRate
   baudRate: 500000,
-  parser: serialport.parsers.readline('\n')
+  parser: serialport.parsers.readline('\n')   //parse recieved lines
 });
 
-port.on('open', function() {
-  console.log('Port opened.');
-  port.write('#\x04C0');
+port.on('open', function() {   // callback to acknoledge that port has been opened
+  console.log('Port opened.'); // print "Port Opened"
+  port.write('#\x04C0');    //send command to RF Explorer
 });
 
-port.on('data', function(data) {
+port.on('data', function(data) {    //Callback that print recieved data 
   console.log('Line received: ' + data);
 });
